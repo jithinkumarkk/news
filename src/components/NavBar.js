191e83c4e60d31bf54185b3b6b48059413bf5917
Jithin Kumar K K <jithinkumarkk13988@gmail.com>
2021-10-15 17:50:43 +0530

    Responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"; 
 export class NavBar extends Component {
-
+  
+      handleChangeCheckBox = event => {
+        document.getElementById("check").checked = false;
+      };
     render() {
         return (
                          <> 
-                             <input type="checkbox" id="check"/>
+                             <input type="checkbox" id="check" checked={this.checked}/>
                                 <label for="check" class="checkbtn">
                                 <i class="fa fa-bars"></i>
                                 </label>
                          <ul className="navbar-nav">
-                            <li className="nav-item"><Link className="nav-link" aria-current="page" for="check" to="/">Home</Link> </li>
-                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li> 
-                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li> 
+                            <li  onClick={this.handleChangeCheckBox}  className="nav-item"><Link className="nav-link" aria-current="page" for="check" to="/">Home</Link> </li>
+                            <li onClick={this.handleChangeCheckBox}   className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                            <li onClick={this.handleChangeCheckBox}   className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li> 
+                            <li onClick={this.handleChangeCheckBox}   className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                            <li onClick={this.handleChangeCheckBox}   className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                            <li onClick={this.handleChangeCheckBox}   className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                            <li onClick={this.handleChangeCheckBox}   className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li> 
                         </ul>
                         </>
 
