0a32c28e46b7044d20d21c5ea18e2766be85bab5
Jithin Kumar K K <jithinkumarkk13988@gmail.com>
2021-10-17 21:03:12 +0530

    final

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,20 +16,35 @@ class SearchBar extends React.Component {
     // this.setState({ searchTopic: event.target.value });
     
   };
+  _handleKeyDown= event =>{
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      this.setState({ searchTopic: event.target.value });
+      this.props.searchForTopic(this.state.searchTopic);
+    }
+  }
   render() {
     return ( 
-        // <Form onSubmit={this.handleSubmit}   style={{ margin:15 }}>
-        //   <Form.Group>
-        //     <Form.Input
-        //       placeholder="Search topic"
-        //       name="topic"
-        //      id="news_search"  
-        //       value={this.state.searchTopic}
-        //       onChange={this.handleChange}
-        //     />
-        //    </Form.Group>
-        // </Form> 
-        <Search placeholder="Search topic"  value={this.state.searchTopic} onSearchChange={this.handleSubmit}  />
+        <Form onSubmit={this.handleSubmit}   style={{ margin:15 }}>
+          <Form.Group>
+            {/* <Form.Input
+              placeholder="Search topic"
+              name="topic"
+             id="news_search"  
+              value={this.state.searchTopic}
+              onChange={this.handleChange}
+            /> */}
+
+            <div class="ui icon input">
+              <input type="text" placeholder="Search News..." id="news_search"  
+              value={this.state.searchTopic}
+              onKeyDown={this._handleKeyDown}
+              onChange={this.handleChange}/>
+              <i class="search icon"></i>
+            </div>
+           </Form.Group>
+        </Form> 
+       // <Search placeholder="Search topic"  value={this.state.searchTopic} onSearchChange={this.handleSubmit}  />
     );
   }
 }
